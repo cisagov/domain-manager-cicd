34166aee498beeaad69833aab493a2efaa68307d
Zachary Wilson <wilsonzachary634@gmail.com>
2020-12-02 10:05:05 -0700

    get rid of unneeded buckets

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,32 +1,8 @@
 # ===========================
 # S3
 # ===========================
-resource "aws_s3_bucket" "websites" {
-  bucket = "${var.app}-${var.env}-websites"
-  acl    = "public-read"
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "Public",
-      "Effect": "Allow",
-      "Principal": "*",
-      "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::${var.app}-${var.env}-websites/*"
-    }
-  ]
-}
-POLICY
-
-  website {
-    index_document = "index.html"
-  }
-}
-
-
 resource "aws_s3_bucket" "bucket" {
-  bucket = "${var.app}-${var.env}-bucket"
+  bucket = "${var.app}-${var.env}"
   acl    = "public-read"
   policy = <<POLICY
 {
@@ -46,9 +22,4 @@ POLICY
   website {
     index_document = "index.html"
   }
-}
-
-resource "aws_s3_bucket" "templates" {
-  bucket = "${var.app}-${var.env}-templates"
-  acl    = "private"
 }
