af885a63ab9d4782b7bae7bb5ece04d84ed14e98
Zachary Wilson <wilsonzachary634@gmail.com>
2021-01-06 09:22:42 -0700

    Enable authentication on API

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -13,19 +13,20 @@ locals {
     "FLASK_APP" : "main"
     "FLASK_ENV" : "development"
     "DEBUG" : 1
+    "DB_PW" : aws_ssm_parameter.docdb_password.value
+    "DB_USER" : aws_ssm_parameter.docdb_username.value
     "DB_HOST" : module.documentdb.endpoint
     "DB_PORT" : 27017
     "MONGO_TYPE" : "DOCUMENTDB"
-    "WEBSITE_STORAGE" : aws_s3_bucket.bucket.id
     "TEMPLATE_BUCKET" : aws_s3_bucket.bucket.id
-    "NC_IP" : "0.0.0.0"
     "BROWSERLESS_ENDPOINT" : "${aws_lb.network.dns_name}:${local.browserless_port}"
     "WORKERS" : 4
     "AWS_REGION" : var.region
     "AWS_DEFAULT_REGION" : var.region
     "API_KEY" : aws_ssm_parameter.api_key.value
-    "DB_PW" : aws_ssm_parameter.docdb_password.value
-    "DB_USER" : aws_ssm_parameter.docdb_username.value
+    "AWS_COGNITO_ENABLED" : 1
+    "AWS_COGNITO_USER_POOL_ID" : aws_cognito_user_pool.pool.id
+    "AWS_COGNITO_USER_POOL_CLIENT_ID" : aws_cognito_user_pool_client.client.id
   }
 
   lambda_environment = {
