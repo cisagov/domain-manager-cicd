e098d807521866f217ba34644071d793daf07f06
Zachary Wilson <wilsonzachary634@gmail.com>
2021-02-24 16:42:31 -0700

    user pool client test

--- terraform/cognito.tf
+++ terraform/cognito.tf
@@ -6,13 +6,13 @@ resource "aws_cognito_user_pool" "pool" {
 }
 
 resource "aws_cognito_user_pool_client" "client" {
-  name                                 = "${var.app}-${var.env}-client"
-  user_pool_id                         = aws_cognito_user_pool.pool.id
-  allowed_oauth_flows                  = ["code"]
-  allowed_oauth_flows_user_pool_client = true
-  allowed_oauth_scopes                 = ["aws.cognito.signin.user.admin", "email", "openid", "phone", "profile"]
-  explicit_auth_flows                  = ["ALLOW_ADMIN_USER_PASSWORD_AUTH", "ALLOW_CUSTOM_AUTH", "ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_PASSWORD_AUTH", "ALLOW_USER_SRP_AUTH"]
-  supported_identity_providers         = ["COGNITO"]
+  name         = "${var.app}-${var.env}-client"
+  user_pool_id = aws_cognito_user_pool.pool.id
+  # allowed_oauth_flows                  = ["code"]
+  # allowed_oauth_flows_user_pool_client = true
+  # allowed_oauth_scopes                 = ["aws.cognito.signin.user.admin", "email", "openid", "phone", "profile"]
+  # explicit_auth_flows                  = ["ALLOW_ADMIN_USER_PASSWORD_AUTH", "ALLOW_CUSTOM_AUTH", "ALLOW_REFRESH_TOKEN_AUTH", "ALLOW_USER_PASSWORD_AUTH", "ALLOW_USER_SRP_AUTH"]
+  # supported_identity_providers         = ["COGNITO"]
 }
 
 resource "aws_cognito_user_pool_domain" "domain" {
