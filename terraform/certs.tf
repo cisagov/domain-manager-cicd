c1714897766c319814a4659412e5be333f2c7edb
Zachary Wilson <zachary.wilson@inl.gov>
2021-04-27 09:29:00 -0600

    add version for certificate

--- terraform/certs.tf
+++ terraform/certs.tf
@@ -3,6 +3,7 @@
 # ===========================
 module "acm" {
   source      = "github.com/terraform-aws-modules/terraform-aws-acm"
+  version     = "~> v2.14.0"
   domain_name = aws_route53_record.domain.name
   zone_id     = data.aws_route53_zone.zone.zone_id
 }
