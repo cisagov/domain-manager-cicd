049b8bc2a492f112101a6d0763b8df1071a4ee6f
Zachary Wilson <zachary.wilson@inl.gov>
2021-06-04 06:31:16 -0600

    allow local contact to docdb

--- cool/docdb.tf
+++ cool/docdb.tf
@@ -34,17 +34,17 @@ resource "aws_ssm_parameter" "docdb_password" {
 # DOCUMENT DB
 # ===========================
 module "documentdb" {
-  source          = "git::https://github.com/cloudposse/terraform-aws-documentdb-cluster.git?ref=tags/0.8.0"
-  stage           = var.env
-  namespace       = var.app
-  name            = "docdb"
-  cluster_size    = var.documentdb_cluster_size
-  master_username = random_string.docdb_username.result
-  master_password = random_password.docdb_password.result
-  instance_class  = var.documentdb_instance_class
-  vpc_id          = local.vpc_id
-  subnet_ids      = local.private_subnet_ids
-  # allowed_cidr_blocks = ["10.0.0.0/8"]
+  source                  = "git::https://github.com/cloudposse/terraform-aws-documentdb-cluster.git?ref=tags/0.8.0"
+  stage                   = var.env
+  namespace               = var.app
+  name                    = "docdb"
+  cluster_size            = var.documentdb_cluster_size
+  master_username         = random_string.docdb_username.result
+  master_password         = random_password.docdb_password.result
+  instance_class          = var.documentdb_instance_class
+  vpc_id                  = local.vpc_id
+  subnet_ids              = local.private_subnet_ids
+  allowed_cidr_blocks     = var.env == "staging" ? ["10.0.0.0/8"] : null
   allowed_security_groups = [aws_security_group.service.id]
   skip_final_snapshot     = true
   tags                    = local.tags
