2a65a506589e827616bfc44740291fb5ffad9faf
mostafa abdo <abdelbaky.mostafa@gmail.com>
2021-08-03 13:50:26 -0500

    update sandbox assume role arn

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -38,7 +38,7 @@ locals {
     "AWS_COGNITO_USER_POOL_CLIENT_ID" : aws_cognito_user_pool_client.client.id
     "AWS_COGNITO_ADMIN_GROUP_NAME" : aws_cognito_user_group.admin.name
     "TWO_CAPTCHA" : data.aws_ssm_parameter.two_captcha.value
-    "SES_ASSUME_ROLE_ARN" : "arn:aws:iam::246048611598:role/SesSendEmail-cyber.dhs.gov"
+    "SES_ASSUME_ROLE_ARN" : var.ses_arn
     "SMTP_FROM" : "domainmanager@cyber.dhs.gov"
     "NEW_USER_NOTIFICATION_EMAIL_ADDRESS" : var.new_user_notification_email_address
     "DEPLOYED_DATE" : var.deployed_date
