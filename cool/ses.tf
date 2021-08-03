718ecaed17e2b8c92f1ce987d1f2d0b23837c860
Zachary Wilson <zachary.wilson@inl.gov>
2021-08-03 14:58:58 -0600

    add rule set resource

--- cool/ses.tf
+++ cool/ses.tf
@@ -12,9 +12,14 @@ resource "aws_lambda_permission" "ses_receive_emails" {
 # ===================================
 # SES Receipt Rule
 # ===================================
+resource "aws_ses_receipt_rule_set" "primary_rule_set" {
+  rule_set_name = "${var.app}-${var.env}-primary"
+
+}
+
 resource "aws_ses_receipt_rule" "domains" {
   name          = "${var.app}-${var.env}-store"
-  rule_set_name = "domains-rule-set"
+  rule_set_name = aws_ses_receipt_rule_set.primary_rule_set.rule_set_name
   recipients    = []
   enabled       = true
   scan_enabled  = true
