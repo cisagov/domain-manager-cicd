9d660827acfa4481374bfdb433b2a14cde627409
Zachary Wilson <zachary.wilson@inl.gov>
2021-11-16 09:11:21 -0700

    upgrade module version

--- terraform/docdb.tf
+++ terraform/docdb.tf
@@ -32,7 +32,7 @@ resource "aws_ssm_parameter" "docdb_password" {
 # DOCUMENT DB
 # ===========================
 module "documentdb" {
-  source                  = "git::https://github.com/cloudposse/terraform-aws-documentdb-cluster.git?ref=tags/0.13.0"
+  source                  = "git::https://github.com/cloudposse/terraform-aws-documentdb-cluster.git?ref=tags/0.14.1"
   stage                   = var.env
   namespace               = var.app
   name                    = "docdb"
