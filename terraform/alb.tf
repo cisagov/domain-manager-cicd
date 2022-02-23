5acb2e8b1427d8cfa4e3052edfc21e603a6c730f
Zachary Wilson <zachary.wilson@inl.gov>
2022-02-23 07:02:38 -0700

    Switch sandbox environment to no longer use alb module

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -13,30 +13,33 @@ resource "aws_security_group" "alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    description = "Allow outbound traffic"
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = {
     "Name" = "${var.app}-${var.env}-alb-sg"
   }
 }
 
-module "alb" {
-  source             = "github.com/cloudposse/terraform-aws-alb"
-  namespace          = var.app
-  stage              = var.env
-  name               = "alb"
-  http_enabled       = false
-  idle_timeout       = var.idle_timeout
+resource "aws_lb" "alb" {
+  name               = "${var.app}-${var.env}-public"
+  idle_timeout       = 600
   internal           = false
-  vpc_id             = var.vpc_id
-  security_group_ids = [aws_security_group.alb.id]
-  subnet_ids         = var.public_subnet_ids
-  target_group_name  = "${var.app}-${var.env}-tg"
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = var.public_subnet_ids
 }
 
 # ===================================
 # Listener
 # ===================================
 resource "aws_lb_listener" "https" {
-  load_balancer_arn = module.alb.alb_arn
+  load_balancer_arn = aws_lb.alb.arn
   port              = 443
   protocol          = "HTTPS"
   certificate_arn   = module.acm.this_acm_certificate_arn
