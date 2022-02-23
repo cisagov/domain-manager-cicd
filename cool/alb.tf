ea80169e7c5f9db2ed4452ce80ed1254653d828e
Zachary Wilson <zachary.wilson@inl.gov>
2022-02-23 07:07:10 -0700

    Switch cool load balancer from module

--- cool/alb.tf
+++ cool/alb.tf
@@ -13,32 +13,33 @@ resource "aws_security_group" "alb" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = -1
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
   tags = merge({
     "Name" = "${var.app}-${var.env}-alb-sg"
   }, local.tags)
 }
 
-module "alb" {
-  source              = "git::https://github.com/cloudposse/terraform-aws-alb.git?ref=tags/0.28.0"
-  namespace           = var.app
-  stage               = var.env
-  name                = "alb"
-  access_logs_enabled = false
-  http_enabled        = false
-  idle_timeout        = var.idle_timeout
-  internal            = true
-  vpc_id              = local.vpc_id
-  security_group_ids  = [aws_security_group.alb.id]
-  subnet_ids          = local.private_subnet_ids
-  target_group_name   = "${var.app}-${var.env}-tg"
-  tags                = local.tags
+resource "aws_lb" "alb" {
+  name               = "${var.app}-${var.env}-internal"
+  idle_timeout       = var.idle_timeout
+  internal           = true
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb.id]
+  subnets            = local.private_subnet_ids
 }
+
 
 # # ===================================
 # # Listener
 # # ===================================
 resource "aws_lb_listener" "https" {
-  load_balancer_arn = module.alb.alb_arn
+  load_balancer_arn = aws_lb.alb.arn
   port              = 443
   protocol          = "HTTPS"
   certificate_arn   = local.cert_arn
